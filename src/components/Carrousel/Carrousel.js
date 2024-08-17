4422b2166690b1b80582208cf065f21fa3a2d043
PedroPablo <pedropablocarrascob@gmail.com>
2024-08-17 19:35:53 -0400

    Se agrega el carrouse en el index

---
+++ src/components/Carrousel/Carrousel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Carrousel = () => {
+   return(
+    <div>
+
+     Carrousel
+       
+    </div>
+   ) 
+}
+
+
+export default Carrousel;
---
+++ src/components/Carrousel/index.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import Carrousel from './Carrousel'
+const App = () => {
+    return (
+        <div>
+            
+            <Carrousel/>
+            Ters
+        </div>
+    )
+}
+
+export default App
